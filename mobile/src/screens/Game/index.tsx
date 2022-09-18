dbcd4f021b8852d9d4ed01a6765fe853211e607a
João Alves <alvezsjv@gmail.com>
2022-09-17 21:05:19 -0300

    feat(mobile): add user connection

--- mobile/src/screens/Game/index.tsx
+++ mobile/src/screens/Game/index.tsx
@@ -12,14 +12,22 @@ import { THEME } from '../../theme';
 import { GameParams } from '../../@types/navigation';
 import { Background } from '../../components/Background';
 import { Heading } from '../../components/Heading';
+import { DuoMatch } from '../../components/DuoMatch';
 import { DuoCard, DuoCardProps } from '../../components/DuoCard';
 
 export function Game() {
   const [duos, setDuos] = useState<DuoCardProps[]>([]);
+  const [discordDuoSelected, setDiscordDuoSelected] = useState('');
 
   const navigation = useNavigation();
   const route = useRoute();
   const game = route.params as GameParams;
+
+  async function getDiscordUser(adsId: string) {
+    fetch(`http://192.168.1.3:3333/ads/${adsId}/discord`)
+      .then(response => response.json())
+      .then(data => setDiscordDuoSelected(data.discord));
+  }
 
   useEffect(() => {
     fetch(`http://192.168.1.3:3333/games/${game.id}/ads`)
@@ -72,7 +80,7 @@ export function Game() {
           renderItem={({ item }) => (
             <DuoCard
               data={item}
-              onConnect={() => {}}
+              onConnect={() => { getDiscordUser(item.id) }}
             />
           )}
           ListEmptyComponent={() => (
@@ -82,7 +90,11 @@ export function Game() {
           )}
         />
 
-
+        <DuoMatch
+          visible={discordDuoSelected.length > 0}
+          discord={discordDuoSelected}
+          onClose={() => setDiscordDuoSelected('')}
+        />
       </SafeAreaView>
     </Background>
   );
